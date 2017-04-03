3bca0693139f9e179df77cec24f991159153cb6f
sebymiano <mianosebastiano@gmail.com>
2017-04-03 09:46:16 +0200

    Added sample file and module types

--- pyang/plugins/swagger.py
+++ pyang/plugins/swagger.py
@@ -328,7 +328,7 @@ def gen_model(children, tree_structure, config=True):
                     allOf = list(node['allOf'])
                     node['items']['allOf'] = allOf
                     del node['allOf']
-                else:
+                elif 'properties' in node:
                     properties = dict(node['properties'])
                     node['items']['properties'] = properties
                     del node['properties']
@@ -381,7 +381,7 @@ def gen_api_node(node, path, apis, definitions, config=True):
         # We take only the schema model of a single item inside the list as a "body"
         # parameter or response model for the API implementation of the list statement.
         if node.keyword == 'list':
-            # Key statement must be present if config statament is True and may
+            # Key statement must be present if config statement is True and may
             # be present otherwise.
             if config:
                 for key in keyList:
