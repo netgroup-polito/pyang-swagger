f21deb1834e27acb7eeeee58cb29892038a20c47
sebymiano <mianosebastiano@gmail.com>
2017-06-26 16:00:18 +0200

    Updated to new pyang version

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ _pwd=$(pwd)
 sudo rm -rf /tmp/pyang-swagger
 mkdir /tmp/pyang-swagger
 
-wget https://github.com/mbj4668/pyang/archive/pyang-1.7.1.tar.gz -O /tmp/pyang-swagger/pyang.tar.gz
+wget https://github.com/mbj4668/pyang/archive/pyang-1.7.2.tar.gz -O /tmp/pyang-swagger/pyang.tar.gz
 mkdir /tmp/pyang-swagger/pyang
 tar -zxf /tmp/pyang-swagger/pyang.tar.gz -C /tmp/pyang-swagger/pyang/ --strip-components=1
 
