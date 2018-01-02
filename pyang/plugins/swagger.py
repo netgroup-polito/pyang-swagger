28027cdaaae1307aa975c87009bb9a2c192839f6
Sebastiano Miano <mianosebastiano@gmail.com>
2018-01-02 02:45:54 +0100

    Added service description and version from YANG model

--- pyang/plugins/swagger.py
+++ pyang/plugins/swagger.py
@@ -247,16 +247,11 @@ def print_header(module, fd, children):
         header['basePath'] = '/'
 
     header['schemes'] = ['http']
-    header['x-iovnet-service-info'] = OrderedDict()
     for attribute in module.substmts:
         if isinstance(attribute.keyword, tuple) and attribute.keyword[1] == "service-description":
-            header['x-iovnet-service-info']['x-service-description'] = attribute.arg
+            header['x-service-description'] = attribute.arg
         elif isinstance(attribute.keyword, tuple) and attribute.keyword[1] == "service-version":
-            header['x-iovnet-service-info']['x-service-version'] = attribute.arg
-
-    if not header['x-iovnet-service-info']:
-        # Dictionary is empty
-        del header['x-iovnet-service-info']
+            header['x-service-version'] = attribute.arg
 
     # Add tags to the header to group the APIs based on every root node found in the YANG
     if len(children) > 0:
